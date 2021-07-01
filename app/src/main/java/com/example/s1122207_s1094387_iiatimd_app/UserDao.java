2e2e0346542f325221db52ccca6872b320725a69
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-07-01 16:06:14 +0200

    User.insertAll(): added onConflictStrategy.IGNORE

--- app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/UserDao.java
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/UserDao.java
@@ -3,10 +3,13 @@ package com.example.s1122207_s1094387_iiatimd_app;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import java.util.List;
+
+//FIXME: Remove  @Insert(onConflict) in production.
 
 @Dao
 public interface UserDao {
@@ -16,10 +19,10 @@ public interface UserDao {
     @Query("SELECT * FROM User where id LIKE :id")
     User getById(int id);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertAll(User... users);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertUser(User user);
 
     @Delete
