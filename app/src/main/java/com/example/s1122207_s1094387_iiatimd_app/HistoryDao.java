87eaa1026c4fdb1d9c62e956d57d312f6771d53e
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-07-01 14:27:42 +0200

    HistoryDao: Added getAll() method

--- app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/HistoryDao.java
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/HistoryDao.java
@@ -1,9 +1,13 @@
 package com.example.s1122207_s1094387_iiatimd_app;
 
 import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface HistoryDao {
     //TODO
-
+    @Query("SELECT * FROM history")
+    List<History> getAll();
 }
