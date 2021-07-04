1e72a2d1f98f5bd947d5c02c92c7c169ab4ec727
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-07-04 17:52:01 +0200

    Replaced createdAt type with Date for the HistoryTableSeeder.

--- app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/AppDatabaseSeeder.java
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/AppDatabaseSeeder.java
@@ -1,5 +1,10 @@
 package com.example.s1122207_s1094387_iiatimd_app;
 
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+
+import java.util.Date;
+
 public class AppDatabaseSeeder {
 
     public static synchronized void insertAllUsersTask(AppDatabase db) {
@@ -12,7 +17,6 @@ public class AppDatabaseSeeder {
 
    public static synchronized void insertAllMedicinesTask(AppDatabase db) {
       Medicine[] medicines = new Medicine[7];
-
       medicines[0] = new Medicine(1, "Ibuprofen", 200d, "mg");
       medicines[1] = new Medicine(2, "Ritalin", 10d, "mg");
       medicines[2] = new Medicine(3, "Adderall", 2.5d, "mg");
@@ -33,5 +37,17 @@ public class AppDatabaseSeeder {
         prescriptions[5] = new Prescription(6, 6, 5, 1, 0);
         prescriptions[6] = new Prescription(7, 7, 2, 1, 0);
         db.prescriptionDao().insertAll(prescriptions);
+    }
+
+    public static synchronized void insertAllHistory(AppDatabase db) {
+        History[] histories = new History[4];
+        long currentTimeMillis = System.currentTimeMillis();
+        Date currentDate = DateConverter.toDate(currentTimeMillis);
+
+        histories[0] = new History(0, 1, 3, 4, currentDate, false);
+        histories[1] = new History(1, 3, 2, 4, currentDate, false);
+        histories[2] = new History(2,2, 2, 4, currentDate, false);
+        histories[3] = new History(3,4, 4, 4, currentDate, false);
+        db.historyDao().insertAllHistory(histories);
     }
 }
