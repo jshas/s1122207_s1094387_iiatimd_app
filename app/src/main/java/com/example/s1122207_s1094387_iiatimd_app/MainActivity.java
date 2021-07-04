15a795c6942570b17b92528c9e1da97f63b3c62c
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-07-04 17:57:31 +0200

    Minor changes to naming.

--- app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/MainActivity.java
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/MainActivity.java
@@ -1,11 +1,45 @@
 package com.example.s1122207_s1094387_iiatimd_app;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.util.Log;
+import android.view.Display;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -27,17 +61,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
         //FIXME:
         // Replace .allowMainThreadQueries() with seperate Task Classes in project
         AppDatabase db = AppDatabase.getInstance(getApplicationContext());
+
         //TODO:
         // MEDICINE ARRAY
         // 1) Replace database seeding with an API FETCH
-        AppDatabaseSeeder.insertAllUsersTask(db);
-        AppDatabaseSeeder.insertAllMedicinesTask(db);
-        AppDatabaseSeeder.insertAllPrescriptions(db);
+//        AppDatabaseSeeder.insertAllUsersTask(db);
+//        AppDatabaseSeeder.insertAllMedicinesTask(db);
+//        AppDatabaseSeeder.insertAllPrescriptions(db);
+//        AppDatabaseSeeder.insertAllHistory(db);
+
+
+
 
         // Gets @string/welcome_text = welcome (var) And adds the User's name at (var).
         String text;
@@ -45,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         nameDisplay.setText(text);
 
 //        mAdapter = new MedicineAdapter(db.medicineDao().getAll());
-        mAdapter = new MedicineAndPrescriptionAdapter(db.medicineDao().getMedicinesWithPrescription());
+        mAdapter = new MedicineAndPrescriptionAdapter(db.prescriptionDao().getMedicinesWithPrescription());
 
         /* MedicineAdapter
          * Used to feed the recyclerView with data from the Medicine table.
