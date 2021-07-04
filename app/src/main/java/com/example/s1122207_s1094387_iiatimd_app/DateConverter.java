f937f06731b2087cb37d88f80a7e72f12bb763b5
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-07-04 17:52:34 +0200

    Added: Date<->Long converter for Room.

---
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/DateConverter.java
@@ -0,0 +1,18 @@
+package com.example.s1122207_s1094387_iiatimd_app;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class DateConverter {
+    @TypeConverter
+    public static Date toDate(Long timestamp) {
+        return timestamp == null ? null : new Date(timestamp);
+    }
+
+    @TypeConverter
+    public static Long toTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+}
+
