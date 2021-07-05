ed6f740d4fdf51e2f5025dee5ab76f63d67f12ae
Kilian <kilianvanembden@gmail.com>
2021-07-05 22:21:35 +0200

    Made login page

--- app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/LoginActivity.java
+++ app/src/main/java/com/example/s1122207_s1094387_iiatimd_app/LoginActivity.java
@@ -1,6 +1,23 @@
 package com.example.s1122207_s1094387_iiatimd_app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener((View v) -> {
+                    Intent loginIntent = new Intent(this, MainActivity.class);
+                    startActivity(loginIntent);
+                }
+        );
+    }
 }
